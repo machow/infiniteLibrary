f85e33cac7e16ed585dd71ddacd26ecbf0870cfa
Jon Berliner <j.s.berliner@gmail.com>
2015-10-17 17:44:47 -0400

    added firstBookLengthN

--- infinite_library.py
+++ infinite_library.py
@@ -7,17 +7,23 @@ from cartesian import cartesianN_repeatSameSet
 pool = list(''.join([string.ascii_letters, string.punctuation, string.digits]))
 cardPool = len(pool)
 
-def bookLength(n):
-    if n in [0,1]: return n
-    else: return int(ceil(log(n, cardPool)))
+def bookLength(i):
+    """gives you the length of book n"""
+    if i in [0,1]: return i  # 0 and 1 do not play nice with log
+    else: return int(ceil(log(i, cardPool)))
 
-def generateBook(n):
-    lenBook = bookLength(n)
-    if lenBook == 0: return ''  # the zero'th book is the empty book :)
+
+def firstBookLengthN(n):
     # determine how many books with len < lenBook exist
-    nShorter = npa([cardPool**l for l in xrange(lenBook)]).sum()
+    nShorter = npa([cardPool**l for l in xrange(n)]).sum()
+    return nShorter  # no need for +1 bc of the empty book
+
+
+def generateBook(i):
+    lenBook = bookLength(i)
+    if lenBook == 0: return ''  # the zero'th book is the empty book :)
     # we're looking for the nThisLength'th book of this length.
-    iThisLength = n - nShorter
+    iThisLength = i - firstBookLengthN(lenBook)
     book = cartesianN_repeatSameSet(pool, lenBook, iThisLength)
     book = ''.join(book)
     return book
